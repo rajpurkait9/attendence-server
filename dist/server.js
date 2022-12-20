e850eb8ff9ee3cc33416421845949c4c30c808cc
raj purkait <rajpurkait9@gmail.com>
2022-12-20 16:29:13 +0530

    first commit

---
+++ dist/server.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("raj purkait the great man");
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(8, 4));
+console.log("something");
+console.log("someting again");
+console.log("raj");
+console.log("again");
+console.log('sldkfjalskdfjasldkfjalskdfj');
+console.log('somsdlfkjasldkfjlaksdjfl');
